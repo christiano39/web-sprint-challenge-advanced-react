2f816efc798a023fa373bb20ad322cdb7418aa5a
christiano39 <carneson39@gmail.com>
2020-07-10 09:53:13 -0600

    added search bar to PlantList

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -5,6 +5,11 @@ export default class PlantList extends Component {
   // add state with a property called "plants" - initialize as an empty array
   state = {
     plants: [],
+    search: '',
+  }
+
+  onInputChange = e => {
+    this.setState({ search: e.target.value })
   }
 
   // when the component mounts:
@@ -12,36 +17,53 @@ export default class PlantList extends Component {
   //   - set the returned plants array to this.state.plants
   componentDidMount() {
     axios.get('http://localhost:3333/plants')
-      .then(res => {this.setState({ plants: res.data.plantsData })})
-      .catch(err => {console.log(err)})
+      .then(res => this.setState({ plants: res.data.plantsData }))
+      .catch(err => console.log(err))
   }
 
   /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
   render() {
     return (
-      <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id}>
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className="plant-bottom-row">
-                <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
-              </div>
-              <button
-                className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
-              </button>
-            </div>
-          </div>
-        ))}
-      </main>
+      <div>
+        <div>
+          <input
+            type='text'
+            value={this.state.search}
+            onChange={this.onInputChange}
+            placeholder='Search for a plant'
+            id='plant-search'
+          />
+        </div>
+        <main className="plant-list">
+          {this.state?.plants?.map((plant) => {
+            if (this.state.search === '' || `${plant.name} ${plant.scientificName}`.toLowerCase().includes(this.state.search.toLowerCase())){
+              return (
+                <div className="plant-card" key={plant.id}>
+                  <img className="plant-image" src={plant.img} alt={plant.name} />
+                  <div className="plant-details">
+                    <h2 className="plant-name">{plant.name}</h2>
+                    <p className="plant-scientific-name">{plant.scientificName}</p>
+                    <p>{plant.description}</p>
+                    <div className="plant-bottom-row">
+                      <p>${plant.price}</p>
+                      <p>☀️ {plant.light}</p>
+                      <p>💦 {plant.watering}x/month</p>
+                    </div>
+                    <button
+                      className="plant-button"
+                      onClick={() => this.props.addToCart(plant)}
+                    >
+                      Add to cart
+                    </button>
+                  </div>
+                </div>
+              )
+            }else{
+              return ''
+            }
+        })}
+        </main>
+      </div>
     );
   }
 }
